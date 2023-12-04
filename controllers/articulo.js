8cf7acdba15f22fe4958623a803de209082b35f3
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2023-12-03 21:32:30 -0500

    subida de archivos, busqueda mediante coincidencias

--- controllers/articulo.js
+++ controllers/articulo.js
@@ -1,6 +1,8 @@
 
 const Articulo = require("../models/Articulo")
 const { validarArticulo } = require("../helpers/validar")
+const fs = require("fs")
+const path = require("path")
 
 const prueba = (req, res) => {
     return res.status(200).json({ mensaje: 'Soy una accion de prueba en mi controlador de articulos' })
@@ -146,9 +148,6 @@ const updateArticulo = (req, res) => {
             mensaje: 'Faltan datos por enviar',
         })
     }
-
-
-
     // Buscar y actualizar articulo
     Articulo.findOneAndUpdate({ _id: id_articulo }, parametros, { new: true }).then((articuloActualizado) => {
 
@@ -165,11 +164,106 @@ const updateArticulo = (req, res) => {
         })
 
     })
+}
+
+const subir = (req, res) => {
+    // Configurar multer
+
+    // Recoger el fichero de imagen subido
+    if (!req.file && !req.files) {
+        return res.status(404).json({
+            status: "error",
+            mensaje: "Peticion Invalida"
+        })
+    }
+    // console.log(req.file)
+
+    // Nombre del archivo
+    const archivo = req.file.originalname;
+
+    // Extensión del archivo
+    const archivo_split = archivo.split("\.");
+    const extension = archivo_split[1];
+
+    // Comprobar extensión correcta 
+    if (extension != "png" && extension != "jpg" && extension != "jpeg" && extension != "gif") {
+        // Borrar archivo y dar respuesta
+        fs.unlink(req.file.path, (error) => {
+            return res.status(400).json({
+                status: "error",
+                mensaje: "Imagen Invalida"
+            })
+        });
+    } else {
+        // Si todo va bien, actualizar el articulo
+        // Recoger el id del articulo a editar
+        const id_articulo = req.params.id
+
+        // Buscar y actualizar articulo
+        Articulo.findOneAndUpdate({ _id: id_articulo }, { imagen: req.file.filename }, { new: true }).then((articuloActualizado) => {
+
+            if (!articuloActualizado) {
+                return res.status(500).json({
+                    status: "error",
+                    mensaje: "Error al actualizar"
+                })
+            }
+            // Devolver respuesta
+            return res.status(200).json({
+                status: 'success',
+                articulo: articuloActualizado,
+                fichero: req.file
+            })
+        })
+    }
+}
+
+const getImagen = (req, res) => {
+    const fichero = req.params.fichero
+    const ruta_fisica = "./imagenes/articulos/" + fichero;
+
+    fs.stat(ruta_fisica, (error, existe) => {
+        if (existe) {
+            return res.sendFile(path.resolve(ruta_fisica))
+        } else {
+            return res.status(404).json({
+                status: "error",
+                mensaje: "La imagen no existe",
+                ruta_fisica
+            })
+        }
+    })
+}
+
+const searchArticulo = async (req, res) => {
+    // Sacar el string de busqueda
+    const busqueda = req.params.busqueda;
+
+    // Find OR 
+    const articulosEncontrado = await Articulo.find({
+        "$or": [
+            { "titulo": { "$regex": busqueda, "$options": "i" } },
+            { "contenido": { "$regex": busqueda, "$options": "i" } },
+
+        ]
+    })
+        .sort({ fecha: -1 })
+        .exec()
+
+    if (!articulosEncontrado || articulosEncontrado.length <= 0) {
+        return res.status(404).json({
+            status: "error",
+            mensaje: "No se ha encontrado articulos"
+        })
+    } else {
+        // Devolver resultado
+        return res.status(200).json({
+            status: "success",
+            articulos: articulosEncontrado
+        })
+    }
 
 
 }
 
-
-
-
-module.exports = { prueba, create, getArticulos, getOneArticulo, deleteArticulo, updateArticulo }
+module.exports = { prueba, create, getArticulos, getOneArticulo, deleteArticulo, updateArticulo, subir, getImagen, searchArticulo }
--- routes/articulo.js
+++ routes/articulo.js
@@ -1,16 +1,31 @@
 const express = require("express")
+const multer = require("multer")
 const router = express.Router();
 
+const almacenamiento = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './imagenes/articulos/');
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, "articulo" + Date.now() + file.originalname);
+    }
+})
+
+const subidas = multer({ storage: almacenamiento });
 const ArticuloController = require("../controllers/articulo");
 
-// Rutas de prueba
 
+// Rutas de prueba
 router.get("/articulos/:ultimos?", ArticuloController.getArticulos)
 router.get("/articulo/:id", ArticuloController.getOneArticulo)
 router.get("/ruta-de-prueba", ArticuloController.prueba)
 router.post("/articulos", ArticuloController.create)
 router.delete("/articulo/:id", ArticuloController.deleteArticulo)
 router.put("/articulo/:id", ArticuloController.updateArticulo)
+router.post("/subirimagen/:id", [subidas.single("file")], ArticuloController.subir)
+router.get("/imagen/:fichero", ArticuloController.getImagen)
+router.get("/buscar/:busqueda", ArticuloController.searchArticulo)
 
 
 
